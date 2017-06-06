69cff316a1251096e5dc093677fcb95b284e8183
Truman Purnell <tru@Trumans-MacBook-Pro.local>
2017-06-06 11:25:27 -0400

    finish svm!!

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -29,19 +29,25 @@ def svm_loss_naive(W, X, y, reg):
   for i in xrange(num_train):
     scores = X[i].dot(W)
     correct_class_score = scores[y[i]]
+    diff_count = 0
     for j in xrange(num_classes):
       if j == y[i]:
         continue
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
+        diff_count += 1
+        dW[:, j] += X[i] # gradient update for incorrect rows
         loss += margin
+    # gradient update for correct row
+    dW[:, y[i]] += -diff_count * X[i]
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
-
+  dW /= num_train
+  dW += reg*W # regularize the weights
   # Add regularization to the loss.
-  loss += reg * np.sum(W * W)
+  loss += 0.5 * reg * np.sum(W * W)
 
   #############################################################################
   # TODO:                                                                     #
@@ -64,18 +70,29 @@ def svm_loss_vectorized(W, X, y, reg):
   """
   loss = 0.0
   dW = np.zeros(W.shape) # initialize the gradient as zero
-
+  num_train = X.shape[0]
+  delta = 1
   #############################################################################
   # TODO:                                                                     #
   # Implement a vectorized version of the structured SVM loss, storing the    #
   # result in loss.                                                           #
   #############################################################################
-  pass
-  #############################################################################
-  #                             END OF YOUR CODE                              #
-  #############################################################################
+  scores = X.dot(W)
+  correct_class_score = scores[np.arange(num_train), y]
+  margins = np.maximum(0, scores - correct_class_score[:, np.newaxis] + delta)
+  margins[np.arange(num_train), y] = 0
+  loss = np.sum(margins)
 
+  loss /= num_train  # get mean
+  loss += 0.5 * reg * np.sum(W * W)  # regularization
 
+  adj_margins = (margins>0).astype(int)
+  num_misclassified = adj_margins.sum(axis=1)
+  adj_margins[np.arange(num_train),y] = -num_misclassified
+
+  dW = X.T.dot(adj_margins)
+  dW /= num_train
+  dW += reg * W
   #############################################################################
   # TODO:                                                                     #
   # Implement a vectorized version of the gradient for the structured SVM     #
